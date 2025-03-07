2ce4bfc1537a6d6635bcc9a8102fe93d8f700ebc
tyleredouglas <58061716+tyleredouglas@users.noreply.github.com>
2025-03-07 12:38:21 -0500

    Update simul_100.py

--- hapML_scripts/simul_100.py
+++ hapML_scripts/simul_100.py
@@ -1,43 +1,38 @@
 import pandas as pd
-import numpy as np
-import pysam
-from Bio import SeqIO
-import random
-import itertools
-from pop_simulator import recombine, simulate_population, get_true_freqs
-from read_simulator import read_fasta, read_coordinates, generate_reads, reverse_complement
+from genome_simulator import GenomeSimulator
+from population_simulator import Simulation
 
 
+# Load and prepare input data
 data = pd.read_csv('/global/scratch/users/tylerdouglas/haplotype_ML/chr3L_RILs_updated.csv')
-df = pd.DataFrame(data)
-df_wide = df.pivot_table(index=['CHROM', 'pos'], columns='sample', values='fHap', aggfunc='first')
+df_wide = data.pivot_table(index=['CHROM', 'pos'], columns='sample', values='fHap', aggfunc='first')
 
-#store founder fastas for each chromosome 
-founders_3L = read_fasta('/global/scratch/users/tylerdouglas/haplotype_ML/fastas/B.3L.fasta')
-
-#list of chromosomes to sample (including only 3L for now)
-chroms = ['chr3L']
-
-#dict so that the correct haplotype can be called from a randomly selected chromosome by read_coordinates
-chromosome_dict = {
-    'chr3L' : founders_3L
-}
-
-#simulation parameters
-n_flies = 300  # population size
-n_generations = 20  # number of generations
-recombination_rate = 0.5  # probability of recombination occurring
+# Define simulation parameters
+n_flies = 300
+n_generations = 20
+recombination_rate = 0.5
 read_num = 500000
-num_sims = 100 # number of populations to simulate
+num_sims = 100
+chromosomes = ['chr3L']
+fasta_files = {'chr3L': '/global/scratch/users/tylerdouglas/haplotype_ML/fastas/B.3L.fasta'}
 
-#simulate populations, generate reads
+# Initialize Genome Simulator
+genome_simulator = GenomeSimulator(fasta_files=fasta_files, chromosomes=chromosomes)
+
+# Run simulations
 for i in range(1, num_sims + 1):
-
     sim_name = f'sim{i}'
-    sim_pop = simulate_population(df_wide, n_flies, n_generations, recombination_rate)
-    true_freqs = get_true_freqs(sim_pop)
-    read_depth, fq1, fq2 = generate_reads(sim_pop, chroms, read_num, sim_name)
-    read_depth.to_csv(f'{sim_name}_depth.csv', index = True)
-    true_freqs.to_csv(f'{sim_name}_true_freqs.csv', index = True)
 
+    # Simulate population genetics
+    population_simulation = Simulation(df_wide, n_flies, n_generations, recombination_rate)
+    simulated_population = population_simulation.run()
 
+    # Calculate true haplotype frequencies
+    true_freqs = population_simulation.calculate_haplotype_frequencies()
+
+    # Generate paired-end reads and calculate read depth
+    read_depth = genome_simulator.generate_paired_end_reads(simulated_population, read_num, sim_name)
+
+    # Save results
+    read_depth.to_csv(f'{sim_name}_depth.csv', index=True)
+    true_freqs.to_csv(f'{sim_name}_true_freqs.csv', index=True)
